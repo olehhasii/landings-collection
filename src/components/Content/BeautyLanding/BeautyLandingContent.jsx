27d20d9bc1e493aeeef7b3501af19f85d79d967e
Oleh Hasii <olehhasii@gmail.com>
2024-06-03 16:22:24 +0300

    added second part to first landing

--- src/components/Content/BeautyLanding/BeautyLandingContent.jsx
+++ src/components/Content/BeautyLanding/BeautyLandingContent.jsx
@@ -8,6 +8,11 @@ import rectangle5 from "../../../assets/BeautyLanding/5.png";
 import rectangle6 from "../../../assets/BeautyLanding/6.png";
 import rectangle7 from "../../../assets/BeautyLanding/7.png";
 import rectangle8 from "../../../assets/BeautyLanding/8.png";
+import natureLeft from "../../../assets/BeautyLanding/nature_left.png";
+import natureRight from "../../../assets/BeautyLanding/nature_right.png";
+import bottom1 from "../../../assets/BeautyLanding/bottom1.png";
+import bottom2 from "../../../assets/BeautyLanding/bottom2.png";
+import bottom3 from "../../../assets/BeautyLanding/bottom3.png";
 
 const BeautyLandingContent = () => {
   const imagesList = [
@@ -67,6 +72,35 @@ const BeautyLandingContent = () => {
         {imagesList.map((image) => (
           <img key={image.id} src={image.src} alt={image.alt} className={styles.grid_image} />
         ))}
+      </div>
+      <div className={styles.second}>
+        <div className={styles.second_wrapper}>
+          <div className={styles.second_content}>
+            <h2 className={styles.second_content_heading}>Discover the beauty around the world</h2>
+            <p className={styles.second_content_text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu.
+            </p>
+            <BaseButton btnText="Get Started" btnClassName={styles.second_content_btn} />
+          </div>
+          <div className={styles.second_sideimages}>
+            <img src={natureLeft} alt="narute left" className={styles.second_sideimages_left} />
+            <img src={natureRight} alt="right left" className={styles.second_sideimages_right} />
+          </div>
+        </div>
+        <div className={styles.second_bottomimages}>
+          <div className={styles.second_bottomimages_block}>
+            <span>01</span>
+            <img src={bottom1} alt="bottom 1" />
+          </div>
+          <div className={styles.second_bottomimages_block}>
+            <span>02</span>
+            <img src={bottom2} alt="bottom 2" />
+          </div>
+          <div className={styles.second_bottomimages_block}>
+            <span>03</span>
+            <img src={bottom3} alt="bottom 3" />
+          </div>
+        </div>
       </div>
     </>
   );
