66aaf6adc347287de15d6c0a0d3d2e963cdee100
Oleh Hasii <olehhasii@gmail.com>
2024-06-07 19:42:23 +0300

    nextcent: added header

--- src/components/Nextcent/Header.jsx
+++ src/components/Nextcent/Header.jsx
@@ -1,5 +1,39 @@
+import styles from "./NextcentStyle.module.scss";
+import logo from "../../assets/Nextcent/Logo.png";
+import BaseButton from "../Buttons/BaseButton";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <ul className={styles.header_nav}>
+        <li className={styles.header_nav_link}>
+          <a href="#">Home</a>
+        </li>
+        <li className={styles.header_nav_link}>
+          <a href="#">Service</a>
+        </li>
+        <li className={styles.header_nav_link}>
+          <a href="#">Feature</a>
+        </li>
+        <li className={styles.header_nav_link}>
+          <a href="#">Product</a>
+        </li>
+        <li className={styles.header_nav_link}>
+          <a href="#">Testimonial</a>
+        </li>
+        <li className={styles.header_nav_link}>
+          <a href="#">FAQ</a>
+        </li>
+      </ul>
+      <div>
+        <BaseButton btnText="Login" btnClassName={styles.header_login} />
+        <BaseButton btnText="Sign Up" btnClassName={styles.header_signup} />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
