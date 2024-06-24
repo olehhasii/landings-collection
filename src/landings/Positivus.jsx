2742f2fb7f45de10e9b1ab38b0523ea43e6e3e6d
Oleh Hasii <olehhasii@gmail.com>
2024-06-24 17:27:30 +0300

    positivus: added case studies section

--- src/landings/Positivus.jsx
+++ src/landings/Positivus.jsx
@@ -1,3 +1,4 @@
+import CaseStudies from "../components/Positivus/CaseStudies";
 import Header from "../components/Positivus/Header";
 import HeroSection from "../components/Positivus/HeroSection";
 import Services from "../components/Positivus/Services";
@@ -8,6 +9,7 @@ const Positivus = () => {
       <Header />
       <HeroSection />
       <Services />
+      <CaseStudies />
     </>
   );
 };
